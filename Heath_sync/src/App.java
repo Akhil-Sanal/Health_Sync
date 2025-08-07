2c7b284da1733870ffb86cc46f500730342ad121
Shinusuresh70 <shinusuresh6@gmail.com>
2025-08-07 21:30:42 +0530

    Doctor registration panel

--- Heath_sync/src/App.java
+++ Heath_sync/src/App.java
@@ -1,6 +1,8 @@
 
 import java.awt.*;
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 
 public class App {
@@ -11,7 +13,7 @@ public class App {
         
        JTabbedPane jtp =new JTabbedPane();
        jtp.addTab("patient",new Patientpanel());
-       jtp.addTab("doctor",new doctorpanel());
+       jtp.addTab("doctor",new doctorpanel(jtp));
       
       frame.add(jtp);
       frame.setVisible(true);
@@ -25,7 +27,7 @@ public class App {
 }
 class Patientpanel extends JPanel{
     Patientpanel(){
-            setLayout(new BorderLayout(10,10));
+        setLayout(new BorderLayout(10,10));
         JButton a=new JButton("Welcome");
         add(a, BorderLayout.NORTH);
 
@@ -41,11 +43,94 @@ class Patientpanel extends JPanel{
     }
 }
 
-    
-
 class doctorpanel extends JPanel{
-    doctorpanel(){
-        JButton b=new JButton("Welcome");
-        add(b);
+       private JTabbedPane parentTabbedPane;
+       doctorpanel(JTabbedPane jtp) {
+        this.parentTabbedPane = jtp;
+        setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
+        JButton welcomeButton = new JButton("Welcome Doctor");
+        JButton registerButton = new JButton("Register Doctor");
+        welcomeButton.setAlignmentX(Component.CENTER_ALIGNMENT);
+        registerButton.setAlignmentX(Component.CENTER_ALIGNMENT);
+        add(Box.createVerticalGlue());
+        add(welcomeButton);
+        add(Box.createRigidArea(new Dimension(0, 10)));
+        add(registerButton);
+        add(Box.createVerticalGlue());
+        registerButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                int existingTabIndex = parentTabbedPane.indexOfTab("Doctor Registration");
+
+                if (existingTabIndex == -1) {
+                    RegistrationPanel regPanel = new RegistrationPanel();
+                    parentTabbedPane.addTab("Doctor Registration", regPanel);
+                    parentTabbedPane.setSelectedComponent(regPanel);
+                } else {
+                    parentTabbedPane.setSelectedIndex(existingTabIndex);
+                }
+            }
+        });
+    }
+}
+class RegistrationPanel extends JPanel {
+    RegistrationPanel() {
+        setLayout(new GridBagLayout());
+        GridBagConstraints gbc = new GridBagConstraints();
+        gbc.insets = new Insets(5, 5, 5, 5);
+        gbc.gridx = 0;
+        gbc.gridy = 0;
+        gbc.anchor = GridBagConstraints.EAST;
+        add(new JLabel("Name:"), gbc);
+        gbc.gridx = 1;
+        gbc.gridy = 0;
+        gbc.fill = GridBagConstraints.HORIZONTAL;
+        gbc.weightx = 1.0;
+        JTextField nameField = new JTextField(20);
+        add(nameField, gbc);
+        gbc.gridx = 0;
+        gbc.gridy = 2;
+        gbc.anchor = GridBagConstraints.EAST;
+        gbc.fill = GridBagConstraints.NONE;
+        gbc.weightx = 0;
+        add(new JLabel("Phone Number:"), gbc);
+        gbc.gridx = 1;
+        gbc.gridy = 2;
+        gbc.fill = GridBagConstraints.HORIZONTAL;
+        gbc.weightx = 1.0;
+        JTextField phoneNoField = new JTextField(20);
+        add(phoneNoField, gbc);
+        gbc.gridx = 0;
+        gbc.gridy = 3;
+        gbc.anchor = GridBagConstraints.EAST;
+        gbc.fill = GridBagConstraints.NONE;
+        gbc.weightx = 0;
+        add(new JLabel("Password:"), gbc);
+        gbc.gridx = 1;
+        gbc.gridy = 3;
+        gbc.fill = GridBagConstraints.HORIZONTAL;
+        gbc.weightx = 1.0;
+        JTextField passwordField = new JTextField(20);
+        add(passwordField, gbc);
+        gbc.gridx = 0;
+        gbc.gridy = 3;
+        gbc.anchor = GridBagConstraints.EAST;
+        gbc.fill = GridBagConstraints.NONE;
+        gbc.weightx = 0;
+        add(new JLabel("Conform Password:"), gbc);
+        gbc.gridx = 1;
+        gbc.gridy = 3;
+        gbc.fill = GridBagConstraints.HORIZONTAL;
+        gbc.weightx = 1.0;
+        JTextField conformpasswordField = new JTextField(20);
+        add(conformpasswordField, gbc);
+        gbc.gridx = 0;
+        gbc.gridy = 4;
+        gbc.gridwidth = 2;
+        gbc.anchor = GridBagConstraints.CENTER;
+        gbc.fill = GridBagConstraints.NONE;
+        gbc.weightx = 0;
+        JButton submitButton = new JButton("Submit");
+        add(submitButton, gbc);
     }
 }
